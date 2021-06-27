45e324cd37fb568c764e7b79b1994512b2d86703
kylejschulz <kylschulz@gmail.com>
2021-06-27 11:03:51 -0600

    add users controller, model, new user request spec, user model spec

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  include SecureRandom
+  after_initialize :assign_api_key, :downcase_email
+
+  has_secure_password
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, confirmation: { case_sensitive: true }
+
+  def assign_api_key
+    self.api_key = SecureRandom.base64.tr('+/=', 'Qrt')
+  end
+
+  def downcase_email
+    if self.email
+      self.email = self.email.downcase
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, model: :type do
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should have_secure_password }
+
+  end
+
+  describe "instance methods" do
+    it "#assign_api_key" do
+      user = User.new(email: 'KYLE@example.com', password: 'password')
+      user.assign_api_key
+
+      expect(user.api_key.length).to eq(24)
+      expect(user.api_key.class).to eq(String)
+
+    end
+
+    it "#downcase_email" do
+      user = User.new(email: 'KYLE@example.com', password: 'password')
+      user.downcase_email
+
+      expect(user.email).to eq("kyle@example.com")
+    end
+  end
+end
